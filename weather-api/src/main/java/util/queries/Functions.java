f3951ac7a7f74a9d4b5426de13ab4159bd1a54fb
Luís Falcão <lfalcao28@gmail.com>
2017-04-05 21:56:58 +0100

    Aula 2017-04-05: Composing Functions: Supplier Lazy with cache.

--- weather-api/src/main/java/util/queries/Functions.java
+++ weather-api/src/main/java/util/queries/Functions.java
@@ -1,8 +1,9 @@
 package util.queries;
 
-import com.sun.istack.internal.NotNull;
-
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -11,8 +12,91 @@ import java.util.function.Supplier;
  * Created by lfalcao on 31/03/2017.
  */
 public class Functions {
-    public static <T> Supplier<T> lazyly(Supplier<T> sup) {
-        return null;
+    static class MyLazySupplier<T> implements Supplier<T> {
+
+        private Supplier<T> longRunningSupplier;
+        T value;
+
+        public MyLazySupplier(Supplier<T> longRunningSupplier) {
+            this.longRunningSupplier = longRunningSupplier;
+        }
+
+        @Override
+        public T get() {
+            if(value == null) {
+                value = longRunningSupplier.get();
+            }
+            return value;
+        }
+    }
+
+    static class MyLazySupplierWithoutIf<T> implements Supplier<T> {
+        T value;
+        private Supplier<T> sup;
+
+        private final Supplier<T> othersCalls = () -> value;
+
+//        Function<Supplier<T>, Supplier<T>> firstCallCreator = (s) ->
+//            () -> {
+//                sup = othersCalls;
+//                value = s.get();
+//                return value;
+//            };
+
+
+
+        public MyLazySupplierWithoutIf(Supplier<T> longRunningSupplier) {
+
+            //sup = firstCallCreator.apply(longRunningSupplier);
+            final Function<Supplier<T>, Supplier<T>> function =
+                    new Function<Supplier<T>, Supplier<T>>() {
+
+                @Override
+                public Supplier<T> apply(Supplier<T> s) {
+
+                    return new Supplier<T>() {
+                        Supplier<T> sup = null;
+                        @Override
+                        public T get() {
+                            MyLazySupplierWithoutIf.this.sup = othersCalls;
+                            value = s.get();
+                            return value;
+                        }
+                    };
+                }
+            };
+
+            sup = function.apply(longRunningSupplier);
+        }
+
+
+
+        @Override
+        public T get() {
+            return sup.get();
+        }
+    }
+    public static <T> Supplier<T> lazilyWithNamedClass(Supplier<T> sup) {
+        return new MyLazySupplier<>(sup);
+    }
+
+    public static <T> Supplier<T> lazilyWithLambdaAndContextCapture(Supplier<T> sup) {
+        final Object[] value = new Object[1];
+        return () -> {
+            if(value[0] == null) {
+                value[0] = sup.get();
+            }
+            return (T)value[0];
+        };
+    }
+
+    public static <T> Supplier<T> lazilyWithOptional(Supplier<T> sup) {
+        final Optional<T>[] value = new Optional[] { Optional.empty() };
+        return () -> value[0].orElseGet(() -> {
+                    value[0] = Optional.of(sup.get());
+                    return value[0].get();
+                }
+        );
     }
 
     public static <T, R> Function<T,R> decorate(Function<T, R> funcToCall, Consumer<T> decorateWith) {
@@ -25,4 +109,5 @@ public class Functions {
         //return (t) -> { R ret = funcToCall.apply(t);  return nextFunction.apply(ret); };
         return (t) -> nextFunction.apply(funcToCall.apply(t));
     }
+
 }
